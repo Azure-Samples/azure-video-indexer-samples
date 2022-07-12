c5f2ddfedb7d7af2a61199154d6af6018ad9cbf6
dupuyjs <jdupuy@microsoft.com>
2022-07-12 18:05:25 +0200

    Add default variables

--- Terraform-Deploy/variables.tf
+++ Terraform-Deploy/variables.tf
@@ -1,11 +1,13 @@
 variable "name" {
   description = "The application name, used to name all resources."
   type        = string
+  default     = "tfvidemo"
 }
 
 variable "environment" {
   description = "The environment name, used to name all resources."
   type        = string
+  default     = "dev"
 
   validation {
     condition     = contains(["dev", "stg", "int", "prd"], var.environment)
@@ -16,14 +18,17 @@ variable "environment" {
 variable "location" {
   description = "The location of all resources."
   type        = string
+  default     = "westeurope"
 }
 
 variable "tenant_id" {
   description = "The tenant id which should be used to deploy resources."
   type        = string
+  default     = "<Your-Tenant-Id>"
 }
 
 variable "subscription_id" {
   description = "The subscription id which should be used to deploy resources."
   type        = string
+  default     = "<Your-Subscription-Id>"
 }

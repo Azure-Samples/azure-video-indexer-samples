344c6efeb95b88992599c4bf16ca4daa5f461f79
Tomer Shaiman <tshaiman@microsoft.com>
2023-10-18 11:26:20 +0300

    fc

---
+++ API-Samples/C#/ArmBased/VideoIndexerClient/Auth1/ArmAccessTokenScope.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VideoIndexingARMAccounts.VideoIndexerClient.Auth
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ArmAccessTokenScope
+    {
+        Account,
+        Project,
+        Video
+    }
+}
---
+++ API-Samples/C#/ArmBased/VideoIndexerClient/Model1/AccountProperties.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace VideoIndexingARMAccounts.VideoIndexerClient.Model;
+
+public class AccountProperties
+{
+    [JsonPropertyName("accountId")]
+    public string Id { get; set; }
+}

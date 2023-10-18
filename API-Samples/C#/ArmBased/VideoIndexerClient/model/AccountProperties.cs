f04db9fde71bd62872cd027476ece3b0d085fe8c
Tomer Shaiman <tshaiman@microsoft.com>
2023-10-18 11:25:59 +0300

    fc

--- API-Samples/C#/ArmBased/VideoIndexerClient/model/AccountProperties.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace VideoIndexingARMAccounts.VideoIndexerClient.Model;
-
-public class AccountProperties
-{
-    [JsonPropertyName("accountId")]
-    public string Id { get; set; }
-}

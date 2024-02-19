23c6a4fd1c760b8c268bf46fbfa6b5c4fefceac8
Tomer Shaiman <tshaiman@microsoft.com>
2024-02-19 21:27:43 +0200

    bicep parameters

--- Deploy-Samples/bicep/deploy.sh
+++ Deploy-Samples/bicep/deploy.sh
@@ -5,19 +5,17 @@ export SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 
 deploy_name=videploy
 #########Fill In The missing Propperties#########
-subscription="24237b72-8546-4da5-b204-8c3cb76dd930"
+subscription="<Your Subscription ID>"
+resource_group="<Your Resource Group Name>"
 location="eastus"
-resource_group="tsviddeploy-99-rg"
 #################################################
-
-
 
 #Template
 parameters_file="main.parameters.json"
 template_file="main.bicep"
 
 # Login to Azure
-#az login --use-device-code
+az login --use-device-code
 az account set -s $subscription
 
 # Create Resource Group
@@ -29,4 +27,3 @@ echo "Deploy Resources"
 az deployment group create -g $resource_group --name $deploy_name \
                         --template-file $template_file \
                         --parameters $parameters_file
-
